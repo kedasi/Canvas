354bb5435c9eaf0ccf2f6be859e1d4ce1916a467
Kerli Edasi <kedasi@itcollege.ee>
2017-03-27 14:16:04 +0300

    adding charts

--- public/scripts.js
+++ public/scripts.js
@@ -10,6 +10,51 @@ window.onload = function () {
     ctx.fillRect(300, 200, 200, 100); //location X Y, width, height
 
     ctx.fillStyle = 'yellow';
-    ctx.arc(100, 100, 50, 0, 2 * Math.PI, false); // x, y, radius, start angle, end angle, clockwise(true)/anticlockwise(false)
+    ctx.arc(100, 100, 50, 0, 2 * Math.PI, false); // x, y, radius, start angle, end angle, anticlockwise
     ctx.fill();
+
+    ctx.strokeStyle = 'blue';
+    ctx.strokeRect(300, 200, 200, 100);
+
+
+    var canvasChart = document.getElementById('canvasChart'); //locate the canvas
+    var ctxChart = canvasChart.getContext('2d'); //access the 2d context (has to be lowercase)
+    var scores = [100, 90, 85, 45, 72, 88];
+    var width = 50;
+    var currX = 50;
+    var base = 200;
+
+    for (var i = 0; i < scores.length; i++) {
+        var height = scores[i];
+        var y = canvasChart.height - height;
+        var fullCircle = 2 * Math.PI;
+        ctxChart.fillStyle = 'coral';
+        ctxChart.fillRect(currX, y, width, height);
+
+        ctxChart.fillStyle = 'teal';
+        ctxChart.beginPath(); // removes connective line
+        ctxChart.arc(currX + width / 2, y, 15, 0, fullCircle, false);
+        ctxChart.fill();
+        ctxChart.strokeStyle = 'white';
+        ctxChart.lineWidth = 2;
+        ctxChart.stroke();
+
+        currX += width + 10;
+    }
+
+    // ctxChart.translate(475, 400);
+    // ctxChart.rotate(Math.PI);
+    // ctxChart.fillStyle = 'coral';
+    // for (var i = 0; i < scores.length; i++) {
+    //     ctxChart.fillRect(currX, 0, width, scores[i]);
+    //     currX += width + 10;
+    // }
+
+    // ctxChart.strokeStyle = 'white';
+    // ctxChart.fillStyle = 'purple';
+    // ctxChart.arc(310, 100, 30, 0, 2 * Math.PI, false);
+    // ctxChart.stroke();
+    // ctxChart.fill();
+
+
 };
