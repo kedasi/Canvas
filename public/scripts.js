d46c2067be64352d1f1be21b8cdb114f23dbd954
Kerli Edasi <kedasi@itcollege.ee>
2017-03-27 11:01:53 +0300

    created circle and rectangle

--- public/scripts.js
+++ public/scripts.js
@@ -2,10 +2,14 @@
  * Created by kedasi on 27/03/2017.
  */
 window.onload = function () {
-    var canvas = document.getElementById('Canvas'); //locate the canvas
-    var ctx = canvas.getContext('2d'); //get the context
+    var canvas = document.getElementById('canvas'); //locate the canvas
+    var ctx = canvas.getContext('2d'); //access the 2d context (has to be lowercase)
 
-    //rendering
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0, 0, 200, 100);
+    //rendering graphics
+    ctx.fillStyle = 'pink';
+    ctx.fillRect(300, 200, 200, 100); //location X Y, width, height
+
+    ctx.fillStyle = 'yellow';
+    ctx.arc(100, 100, 50, 0, 2 * Math.PI, false); // x, y, radius, start angle, end angle, clockwise(true)/anticlockwise(false)
+    ctx.fill();
 };
